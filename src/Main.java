1d248b8c12eab48518cd2319f5152eb61ff80c61
folasadea <fadegokea@gmail.com>
2025-08-04 15:31:57 +0100

    strength, common passwords

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
-
-    public void length() {
-
-    }
-    
     public static void main(String[] args) {
         //scanner and initialisation
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your password");
         String password = sc.nextLine();
-        int plength = 0;
         int score = 0;
 
+
         //password length has to be at least 8 characters
-        for(int i=1;i<=password.length(); i++) {
-            plength++;
-        }
-        if(plength < 8) {
+        if(password.length() < 8) {
             System.out.println("Your password must be at least 8 characters");
         } else {
             score++;
@@ -40,7 +32,7 @@ public class Main {
 
         //password has to contain at least lowercase letter
         if(!password.matches(".*[a-z].*")) {
-            System.out.println("Your password must contain at least one uppercase letter");
+            System.out.println("Your password must contain at least one lowercase letter");
         } else {
             score++;
         }
@@ -51,6 +43,20 @@ public class Main {
         } else {
             score++;
         }
-        System.out.println("Your password strength is " + score + "/5");
+
+        if (password.contains("123456") || password.contains("123456789") || password.contains("qwerty") || password.contains("password") || password.contains("11111111") ||
+                password.contains("12345678") || password.contains("abc123") || password.contains("1234567") || password.contains("password1") || password.contains("12345") ||
+                password.contains("1234567890") || password.contains("123123") || password.contains("000000") || password.contains("Iloveyou") || password.contains("1234") ||
+                password.contains("1q2w3e4r5t") || password.contains("Qwertyuiop") || password.contains("123") || password.contains("Monkey") || password.contains("Dragon")) {
+            System.out.println("Your password strength is 0/5: extremely weak");
+        }
+
+        if (score == 1||score == 2) {
+            System.out.println("Your password strength is " + score + "/5: weak.");
+        } else if(score == 3) {
+            System.out.println("Your password strength is " + score + "/5: medium.");
+        } else if (score == 4|| score == 5) {
+            System.out.println("Your password strength is " + score + "/5: strong.");
+        }
     }
 }
